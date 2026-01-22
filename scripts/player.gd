f7a14b3bfcce0bf6df9984baab1c3ee8d67c1055
HiImTonyy <RoxRox876@gmail.com>
2026-01-21 22:06:24 -0500

    Added player variables.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,3 +2,10 @@ extends Node
 
 var first_name = "Tony"
 var last_name = "Sepreoni"
+var cash : float = 0.00
+enum rank {noobie, junior, officer, senior_officer, master}
+var daily_wage : float = 50.00
+var went_to_work : bool = false
+enum hunger_level {dying, very_hugry, hungry, alright, great, full, fatso}
+var times_ate_today : int = 0
+var total_food_items : int = 0
