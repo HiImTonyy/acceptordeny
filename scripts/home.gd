c598c75b1601f4afdcbec855e002037fc87a399b
HiImTonyy <RoxRox876@gmail.com>
2026-01-22 01:01:25 -0500

    Added the top u.i in home.

--- scripts/home.gd
+++ scripts/home.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var label_player_name : Label
+@export var label_player_cash : Label
+@export var label_hunger_level : Label
+
 func _ready():
-	print(player.first_name)
-	print(player.last_name)
+	label_player_name.text = "Name: " + player.first_name + " " + player.last_name
+	label_player_cash.text = "Balance: " + "$" + str(snapped(player.cash, 0.01))
+	label_hunger_level.text = "Hunger Level: " + player.current_hunger_level
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,13 @@ extends Node
 
 var first_name = "Tony"
 var last_name = "Sepreoni"
-var cash : float = 0.00
+var cash : float = 250.97
 enum rank {noobie, junior, officer, senior_officer, master}
 var current_rank = rank.noobie
-var daily_wage : float = 50.00
+var daily_wage : float = 50
 var went_to_work : bool = false
 enum hunger_level {dying, very_hugry, hungry, alright, great, full, fatso}
-var current_hunger_level = hunger_level.great
+var current_hunger_level_setting = hunger_level.great
+var current_hunger_level = "great"
 var times_ate_today : int = 0
 var total_food_items : int = 0
